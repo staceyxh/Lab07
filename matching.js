fd931b94a97c3ee9c31658d9a16a74894d98a483
Kaeman Filiatrault <kaeman.filiatrault@centre.edu>
2020-03-04 16:06:24 -0500

    final commit fro js project

--- matching.js
+++ matching.js
@@ -86,12 +86,15 @@ d3.select("body")
             d3.selectAll("body")
                 .append("img")
                 .attr("src", "explosion.png")
+                .transition().duration(5000).ease(d3.easeElastic).style("opacity",1)
                 .style("position", "fixed")
                 .style("width", "400px")
                 .style("height", "250px")
                 .style("left", "0px")
                 .style("top", "30px")
-    
+            d3.select("body p")
+                .text("YOU LOSE FINAL SCORE: " + total)
+                .style("font-family","Impact")   
         })
 d3.select("body")
   .append("img")
@@ -135,55 +138,90 @@ d3.select("body")
     .attr("class", "squirtle")
     .style("width", "50px")
     .style("height", "50px")
+var total = 0;
+var scoreCounter = function(total, points)
+{
+    return total + points;
+    
+}
+
+
 d3.selectAll("body img.charmander")
     .on("click", function()
        {
+            total = scoreCounter(total, 5)
             d3.select(this)
                 .attr("src", "charmander.png")
+            d3.select("body p")
+                .text("Score: "+total)
     
     
         })
 d3.selectAll("body img.tuby")
     .on("click", function()
        {
+            total = scoreCounter(total, 10)
             d3.select(this)
                 .attr("src", "tuby.jfif")
+            d3.select("body p")
+                .text("Score: "+total)
     
     
         })
 d3.selectAll("body img.grass")
     .on("click", function()
        {
+            total = scoreCounter(total, -15)
             d3.select(this)
                 .attr("src", "grass.png")
+             d3.select("body p")
+                .text("Score: "+total)
     
     
         })
 d3.selectAll("body img.elephant")
     .on("click", function()
        {
+            total = scoreCounter(total, 20)
             d3.select(this)
                 .attr("src", "elephant.jfif")
+             d3.select("body p")
+                .text("Score: "+total)
     
         })
 d3.selectAll("body img.monkey")
     .on("click", function()
        {
+            total = scoreCounter(total, 25)
             d3.select(this)
                 .attr("src", "monkey.jfif")
+             d3.select("body p")
+                .text("Score: "+total)
     
         })
 d3.selectAll("body img.picachu")
     .on("click", function()
        {
+            total = scoreCounter(total, 50)
             d3.select(this)
                 .attr("src", "picachu.jfif")
+             d3.select("body p")
+                .text("Score: "+total)
     
         })
 d3.selectAll("body img.squirtle")
     .on("click", function()
        {
+            total = scoreCounter(total, 30)
             d3.select(this)
                 .attr("src", "squirtle.png")
+             d3.select("body p")
+                .text("Score: "+total)
     
         })
+
+d3.select("body")
+    .append("p")
+    .text("Score: ")
+    .style("font-size", "40")
+    
